6572087d489293dc3dc78ad1cd89616c55d980ca
Gabriel Melo <galase0408@gmail.com>
2023-01-06 17:44:00 -0300

    test pwa on ios

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,27 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="manifest" href="/manifest.json" />
+
+        <link rel="apple-touch-icon" href="./assets/icon-180-180.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="./assets/icon-152-152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="./assets/icon-180-180.png'"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="./assets/icon-167-167.png"
+        />
+
+        <meta name="apple-mobile-web-app-title" content="Lorem Ipsum" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       </Head>
       <script type="text/javascript" src="/static/script.js"></script>
       <Component {...pageProps} />
